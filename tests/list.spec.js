5c4d40069b4b281c6d57e337f7a5fba501554097
Rahul Gaur <rahul.nbg@gmail.com>
2018-07-09 20:28:20 +0530

    chore: add more testcases

---
+++ tests/list.spec.js
@@ -0,0 +1,107 @@
+const o = require("ospec");
+const { AppShell } = require("../bundle");
+
+new function(o) {
+  let clone = o.new();
+  let describe = clone.spec;
+  let it = clone;
+  let expect = clone;
+  let spyOn = clone.spy;
+
+  /**
+   * lazyioc List test suite
+   */
+  describe("lazyioc#list", function() {
+    it("will return an empty array if no container is provided", function() {
+      const lazyioc = new AppShell();
+      expect(lazyioc.list() instanceof Array).equals(true);
+      expect(lazyioc.list().length).equals(0);
+    });
+    it("will return a list of registered services from a passed container", function() {
+      const lazyioc = new AppShell();
+      lazyioc.service("A", function() {});
+      lazyioc.service("B", function() {});
+
+      const list = lazyioc.list(lazyioc.container);
+      expect(list.indexOf("A")).notEquals(-1);
+      expect(list.indexOf("B")).notEquals(-1);
+    });
+
+    it("will exclude shell methods from the returned list", function() {
+      const lazyioc = new AppShell();
+      lazyioc.service("A", function() {});
+      lazyioc.service("B", function() {});
+
+      const list = lazyioc.list(lazyioc.container);
+      expect(list.indexOf("$register")).equals(-1);
+      expect(list.indexOf("$list")).equals(-1);
+      expect(list.length).equals(2);
+    });
+  });
+
+  describe("prototype#list", function() {
+    it("will return an empty array if no services have been registered", function() {
+      const lazyioc = new AppShell();
+      expect(lazyioc.list() instanceof Array).equals(true);
+      expect(lazyioc.list().length).equals(0);
+    });
+    it("will return a list of registered services", function() {
+      const lazyioc = new AppShell();
+      lazyioc.service("A", function() {});
+      lazyioc.service("B", function() {});
+
+      const list = lazyioc.list();
+      expect(list.indexOf("A")).notEquals(-1);
+      expect(list.indexOf("B")).notEquals(-1);
+    });
+    it("will exclude container methods from the returned list", function() {
+      const lazyioc = new AppShell();
+      lazyioc.service("A", function() {});
+      lazyioc.service("B", function() {});
+
+      const list = lazyioc.list();
+      expect(list.indexOf("$register")).equals(-1);
+      expect(list.indexOf("$list")).equals(-1);
+      expect(list.length).equals(2);
+    });
+  });
+  describe("container#$list", function() {
+    it("will return an empty array if no services have been registered", function() {
+      const lazyioc = new AppShell();
+      expect(lazyioc.container.$list() instanceof Array).equals(true);
+      expect(lazyioc.container.$list().length).equals(0);
+    });
+    it("will return a list of registered services", function() {
+      const lazyioc = new AppShell();
+      lazyioc.service("A", function() {});
+      lazyioc.service("B", function() {});
+
+      const list = lazyioc.container.$list();
+      expect(list.indexOf("A")).notEquals(-1);
+      expect(list.indexOf("B")).notEquals(-1);
+    });
+    it("will exclude container methods from the returned list", function() {
+
+      const lazyioc = new AppShell();
+      lazyioc.service("A", function() {});
+      lazyioc.service("B", function() {});
+
+      const list = lazyioc.container.$list();
+      expect(list.indexOf("$register")).equals(-1);
+      expect(list.indexOf("$list")).equals(-1);
+      expect(list.length).equals(2);
+    });
+    it("will work with nested containers", function() {
+      
+      const lazyioc = new AppShell();
+      lazyioc.service("test.A", function() {});
+      lazyioc.service("test.B", function() {});
+
+      const list = lazyioc.container.test.$list();
+      expect(list.indexOf("A")).notEquals(-1);
+      expect(list.indexOf("B")).notEquals(-1);
+    });
+  });
+
+  clone.run();
+}(o);
---
+++ tests/middleware.spec.js
@@ -0,0 +1,113 @@
+const o = require("ospec");
+const { AppShell } = require("../bundle");
+
+new function(o) {
+  let clone = o.new();
+  let describe = clone.spec;
+  let it = clone;
+  let expect = clone;
+  let spyOn = clone.spy;
+
+  /**
+   * lazyIoc Middleware test suite
+   */
+  describe("lazyIoc#middleware", function() {
+    it("middleware get executed every time a service is accessed", function() {
+      const lazyIoc = new AppShell();
+      let count = 0;
+      lazyIoc.service("Thing", function() {
+        this.name = "Thing";
+      });
+      lazyIoc.middleware("Thing", function(service, next) {
+        count++;
+        next();
+      });
+      expect(count).equals(0);
+      expect(lazyIoc.container.Thing).notEquals(undefined);
+      expect(count).equals(1);
+      expect(lazyIoc.container.Thing).notEquals(undefined);
+      expect(count).equals(2);
+    });
+    it("middleware get the service as the first param", function() {
+      const lazyIoc = new AppShell();
+      lazyIoc.service("Thing", function() {
+        this.name = "Thing";
+      });
+      lazyIoc.middleware("Thing", function(service, next) {
+        service.name = "Something New";
+        next();
+      });
+      expect(lazyIoc.container.Thing.name).equals("Something New");
+    });
+
+    it("generic middleware run for all services", function() {
+      const lazyIoc = new AppShell();
+      lazyIoc.service("Thing1", function() {
+        this.name = "Thing1";
+      });
+      lazyIoc.service("Thing2", function() {
+        this.name = "Thing2";
+      });
+      lazyIoc.middleware(function(service, next) {
+        service.name = "Changed";
+        next();
+      });
+      expect(lazyIoc.container.Thing1.name).equals("Changed");
+      expect(lazyIoc.container.Thing2.name).equals("Changed");
+    });
+
+    it("can handle dot notation keys", function() {
+      const lazyIoc = new AppShell();
+      lazyIoc.service("Util.Thing", function() {
+        this.name = "Util Thing";
+      });
+      lazyIoc.middleware("Util.Thing", function(service, next) {
+        service.name = "Middleware Thing";
+        next();
+      });
+      expect(lazyIoc.container.Util.Thing.name).equals("Middleware Thing");
+    });
+
+    it("can handle deeply nested dot notation keys", function() {
+      const lazyIoc = new AppShell();
+      lazyIoc.service("Util.A.B.C.Thing", function() {
+        this.name = "Util Thing";
+      });
+      lazyIoc.middleware("Util.A.B.C.Thing", function(service, next) {
+        service.name = "Middleware Thing";
+        next();
+      });
+      expect(lazyIoc.container.Util.A.B.C.Thing.name).equals("Middleware Thing");
+    });
+
+    it("can register middleware before the service", function() {
+      const lazyIoc = new AppShell();
+      lazyIoc.middleware("Util.A.B.C.Thing", function(service, next) {
+        service.name = "Middleware Thing";
+        next();
+      });
+      lazyIoc.service("Util.A.B.C.Thing", function() {
+        this.name = "Util Thing";
+      });
+      expect(lazyIoc.container.Util.A.B.C.Thing.name).equals("Middleware Thing");
+    });
+
+    it("throw error when next(err)", function() {
+      const lazyIoc = new AppShell();
+      lazyIoc.service("Thing", function() {
+        this.name = "Thing";
+      });
+      this.e = new Error("Thing error");
+      let context = this;
+      lazyIoc.middleware("Thing", function(service, next) {
+        next(context.e);
+      });
+      try {
+        lazyIoc.container.Thing.name;
+      } catch (error) {
+        expect(error).equals(context.e);  
+      }
+    });
+  });
+  clone.run();
+}(o);
